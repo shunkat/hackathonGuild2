a4f6d6ac4ce533de9353974d3837966cce9e6f11
shunkat <shunkat@icloud.com>
2024-02-11 01:44:32 +0900

    set up basic screens and screenModel

---
+++ composeApp/src/commonMain/kotlin/guild/hackathon/ui/List/ListScreenModel.kt
@@ -0,0 +1,10 @@
+package guild.hackathon.ui.List
+
+import cafe.adriel.voyager.core.model.ScreenModel
+import guild.hackathon.net.UserApi
+
+class ListScreenModel(
+    userApi: UserApi
+): ScreenModel {
+
+}
