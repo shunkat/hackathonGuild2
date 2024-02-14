040893a50eae8e721c430d5ef734185e29eae7ce
shunkat <shunkat@icloud.com>
2024-02-14 20:52:44 +0900

    updateUI of ListScreen

--- composeApp/src/commonMain/kotlin/guild/hackathon/ui/List/ListScreen.kt
+++ composeApp/src/commonMain/kotlin/guild/hackathon/ui/List/ListScreen.kt
@@ -7,9 +7,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.LocalNavigator
@@ -25,7 +28,13 @@ class ListScreen() : Screen {
         val screenModel = getScreenModel<ListScreenModel>()
         val hackathons = screenModel.hackathons.value
 
-        Column(modifier = Modifier.padding(top = 16.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = "ハッカソン一覧",
+                fontFamily = rememberCustomFontStyle(),
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
             LazyColumn(contentPadding = PaddingValues(top = 16.dp)) {
                 items(hackathons) { hackathon ->
                     Row(
@@ -33,9 +42,24 @@ class ListScreen() : Screen {
                             navigator.push(DetailScreen(hackathon))
                         }
                     ) {
-                        Text(text = hackathon.title, fontFamily = rememberCustomFontStyle())
-                        Text(text = hackathon.duration, fontFamily = rememberCustomFontStyle())
+                        Column(
+                            modifier = Modifier.clickable {
+                                navigator.push(DetailScreen(hackathon))
+                            }
+                        ) {
+                            Text(
+                                text = hackathon.title,
+                                fontFamily = rememberCustomFontStyle(),
+                                modifier = Modifier.align(Alignment.Start)
+                            )
+                            Text(
+                                text = hackathon.duration,
+                                fontFamily = rememberCustomFontStyle(),
+                                modifier = Modifier.align(Alignment.Start).padding(start = 32.dp)
+                            )
+                        }
                     }
+                    Divider()
                 }
             }
         }
