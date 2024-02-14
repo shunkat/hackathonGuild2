b1c63fac650f8bb55453daec1b49ccade6015144
shunkat <shunkat@icloud.com>
2024-02-14 20:53:08 +0900

    update UI of detail screen

--- composeApp/src/commonMain/kotlin/guild/hackathon/ui/Detail/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/guild/hackathon/ui/Detail/DetailScreen.kt
@@ -1,8 +1,10 @@
 package guild.hackathon.ui.Detail
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +13,7 @@ import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import guild.hackathon.di.getScreenModel
 import guild.hackathon.model.Hackathon
+import guild.hackathon.openUrl
 import guild.hackathon.theme.rememberCustomFontStyle
 
 class DetailScreen(private val hackathon: Hackathon): Screen {
@@ -18,19 +21,23 @@ class DetailScreen(private val hackathon: Hackathon): Screen {
     override fun Content() {
         val screenModel = getScreenModel<DetailScreenModel>()
 
-        Column {
-            Text(text = "ID: ${hackathon.id}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Created At: ${hackathon.createdAt}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Title: ${hackathon.title}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Duration: ${hackathon.duration}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Apply Finish Date: ${hackathon.applyFinishDate}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Detail: ${hackathon.detail}", fontFamily = rememberCustomFontStyle())
-            Text(text = "Detail URL: ${hackathon.detailUrl}", fontFamily = rememberCustomFontStyle())
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "作成日: ${hackathon.createdAt}", fontFamily = rememberCustomFontStyle())
+            Text(text = "タイトル: ${hackathon.title}", fontFamily = rememberCustomFontStyle())
+            Text(text = "期間: ${hackathon.duration}", fontFamily = rememberCustomFontStyle())
+            Text(text = "申し込み締切日: ${hackathon.applyFinishDate}", fontFamily = rememberCustomFontStyle())
+            Text(text = "詳細: ${hackathon.detail}", fontFamily = rememberCustomFontStyle())
+            Text(
+                text = "詳細URL: ${hackathon.detailUrl}",
+                fontFamily = rememberCustomFontStyle(),
+                modifier = Modifier.clickable { openUrl(hackathon.detailUrl) }
+            )
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Button(onClick = { /* Handle click here */ }) {
-                Text(text = "チーム募集を開始する")
+            Button(onClick = {
+            }) {
+                Text(text = "チーム募集を開始する", fontFamily = rememberCustomFontStyle())
             }
         }
     }
